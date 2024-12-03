f250abcf0585470b5d7d4e854049d694cc83a097
arpo008 <arporoy008@gmail.com>
2024-12-04 00:45:54 +0600

    Conflict

--- Backend/src/services/logInService.ts
+++ Backend/src/services/logInService.ts
@@ -1,10 +1,9 @@
-
 import DatabaseSingleton from '../database/index';
 import bcrypt from 'bcrypt';
 import { loginQuery } from '../queries/userQueries';
 import { loginUser } from '../queries/userQueries';
 import { generateToken } from '../services/handleJWTService';
-import { Request, Response } from 'express';    
+import { Request, Response } from 'express';
 
 export const logInService = async (user_id: number, providedPassword: string) => {
     try {
@@ -23,21 +22,21 @@ export const logInService = async (user_id: number, providedPassword: string) =>
             delete userData.password;
 
             const database = DatabaseSingleton.getInstance();
-            const client = database.getClient();   
-            
+            const client = database.getClient();
+
             const clockInTime = new Date().toISOString();
             let staus = await client.query(loginQuery, [user_id, 'active']);
 
-            if (staus.rows.length === 0) {
+            if (staus.rowCount === 0) {
                 return { error: 'An error occurred during login' };
             }
 
             const token = generateToken(userData);
             return { message: 'Login successful', web_tokens: token };
         }
-        
+
     } catch (error) {
         console.error('Error during login:', error);
         return { error: 'An error occurred during login' };
     }
-};
+}
