7d6d8fad3cf049b7c666a5ed0453266fd16ef2b9
arpo008 <arporoy008@gmail.com>
2024-12-04 12:48:25 +0600

    get All user Data, update Eployee List

--- Backend/src/services/logInService.ts
+++ Backend/src/services/logInService.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import { loginQuery } from '../queries/userQueries';
 import { loginUser } from '../queries/userQueries';
 import { generateToken } from '../services/handleJWTService';
-import { Request, Response } from 'express';
+import { Request, Response } from 'express';    
 
 export const logInService = async (user_id: number, providedPassword: string) => {
     try {
@@ -23,26 +23,20 @@ export const logInService = async (user_id: number, providedPassword: string) =>
             delete userData.image;
 
             const database = DatabaseSingleton.getInstance();
-<<<<<<< HEAD
-            const client = database.getClient();
-
-            const clockInTime = new Date().toISOString();
-=======
             const client = database.getClient();   
             
->>>>>>> 6fa3212725685b236cd0b5af781625d5e870dacd
             let staus = await client.query(loginQuery, [user_id, 'active']);
 
-            if (staus.rowCount === 0) {
+            if (staus.rows.length === 0) {
                 return { error: 'An error occurred during login' };
             }
 
             const token = generateToken(userData);
             return { message: 'Login successful', web_tokens: token };
         }
-
+        
     } catch (error) {
         console.error('Error during login:', error);
         return { error: 'An error occurred during login' };
     }
-}
+};
